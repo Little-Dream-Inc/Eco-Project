9566b1f3577bcd50fbdb5cbe41f43e678c840008
Eldarian <eldarian3@gmail.com>
2021-04-16 10:56:01 +0300

    Suffers with rails
    Unfinished rail movement

---
+++ Assets/Inputs.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour
+{
+    Cart currentCart;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+                if(hit.collider.GetComponent<Cart>()) // ==null?
+                {
+                    currentCart = hit.collider.GetComponent<Cart>();
+                    startPos = currentCart.gameObject.transform.position;
+                }
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            currentCart = null;
+        }
+
+        if(Input.GetMouseButton(0) && currentCart != null)
+        {
+            currentCart.Move(Camera.main.ScreenToWorldPoint(Input.mousePosition), startPos);
+        }
+    }
+}
